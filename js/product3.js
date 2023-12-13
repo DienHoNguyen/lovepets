452bf7c1a57e4f1e1e29fccefd7d44440154c166
DienHoNguyen91 <110595893+DienHoNguyen@users.noreply.github.com>
2023-12-13 15:12:40 +0630

    Add files via upload

---
+++ js/product3.js
@@ -0,0 +1,68 @@
+export const products = [
+    {
+        id: "b1",
+        image: 'shop2/f1.jpg',
+        name: "Đồ ăn khô Pedigree cho chó",
+        price: 120000,
+        description: "Đồ ăn khô",
+        keywords: [
+            "dried",
+            "food",
+            "dog"
+        ]
+}, {
+        id: "b2",
+        image: 'shop2/f2.jpg',
+        name: "Đồ ăn khô Meow Mix cho mèo",
+        price: 3200000,
+        description: "Đồ ăn khô",
+        keywords: [
+            "dried",
+            "food",
+            "cat"
+        ]  
+}, {
+        id: "b3",
+        image: 'shop2/f3.jpg',
+        name: "Đồ ăn khô Science Diet cho chó",
+        price: 8000000,
+        description: "Đồ ăn khô",
+        keywords: [
+            "dried",
+            "food",
+            "dog"
+        ] 
+}, {
+        id: "b4",
+        image: 'shop2/t1.jpg',
+        name: "Đồ chơi Multiplet Loofa",
+        price: 50000,
+        description: "Đồ chơi cho thú cưng",
+        keywords: [
+            "toy",
+            "plastic",
+            "dog"
+        ] 
+}, {
+        id: "b5",
+        image: 'shop2/l1.jpg',
+        name: "Dây lifetime cho thú cưng",
+        description: "Dây dắt thú cưng",
+        price: 75000,
+        keywords: [
+            "leash",
+            "pet",
+            "dog"
+        ] 
+}, {
+        id: "b6",
+        image: 'shop2/t2.jpg',
+        name: "Bóng nhựa Kong cho thú cưng",
+        description: "Đồ chơi cho thú cưng",
+        price: 56000,
+        keywords: [
+            "ball",
+            "plastic",
+            "dog"
+        ] 
+}];
