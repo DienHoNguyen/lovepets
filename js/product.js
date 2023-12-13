64b7f21d342d54876037e375bb0e31386f6b5941
DienHoNguyen91 <110595893+DienHoNguyen@users.noreply.github.com>
2023-12-13 14:59:03 +0630

    Update product.js

--- js/product.js
+++ js/product.js
@@ -59,34 +59,5 @@ export const pets = [
             "alaska",
             "dog"
         ] 
-},{
-    id: "b1",
-    image: 'shop2/f1.jpg',
-    name: "Đồ ăn khô Pedigree cho chó",
-    price: 120000,
-    keywords: [
-        "dried",
-        "food",
-        "dog"
-    ]
-}, {
-    id: "b2",
-    image: 'shop2/f2.jpg',
-    name: "Đồ ăn khô Meow Mix cho mèo",
-    price: 3200000,
-    keywords: [
-        "dried",
-        "food",
-        "cat"
-    ]  
-}, {
-    id: "b3",
-    image: 'shop2/f3.jpg',
-    name: "Đồ ăn khô Science Diet cho chó",
-    price: 8000000,
-    keywords: [
-        "dried",
-        "food",
-        "dog"
-    ] 
-},];
+},
+,];
